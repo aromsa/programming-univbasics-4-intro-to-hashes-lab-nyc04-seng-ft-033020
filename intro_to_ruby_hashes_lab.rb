e4f6dc8b0bde0c9d386d333c24cd3cec00a9dd6d
anna bateman <annacbateman@gmail.com>
2020-03-24 16:51:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   @key = key
-  @value = value
+  @value = value 
+  hash = 
   
 end
 
